449dfd89291759bf205bbc384b35ec688fd5aea1
sachin.vashist <sachin.vashist@vodafone.com>
2023-06-21 16:18:38 +0530

    script ready

--- index.js
+++ index.js
@@ -1,61 +1,79 @@
 const Crawler = require('crawler');
 const range = require('lodash/range')
+const validUrl = require('valid-url');
 const saveToExcel = require('./saveToExcel')
 const each = require('async-each-series');
 const path1 = "/Users/sachin.a.vashist/Desktop/crawler/output1.xlsx";
 const path2 = "/Users/sachin.a.vashist/Desktop/crawler/output2.xlsx";
 
-
 const siteDespCrawler = new Crawler({
     maxConnections: 10,
-    callback: (error, res, done) => {
-        if (error) return error
-        const $ = res.$;
-        let name = $('.search-page-heading-red').text(),
-            details = $('.detail-line'),
-            website = details.find('.pglink').attr('href'),
-            phone = details.clone().children().remove().end().text()
-        saveToExcel(path2, [[name, website, phone]], 'website')
-
-        done();
-    }
 });
-
-
 const siteCrawler = new Crawler({
-    maxConnections: 10,
-    callback: (error, res, done) => {
-        if (error) return error
-        const $ = res.$;
-        let out = $('.search-result-left')
-        let outArray = []
-        out.each(function (i, el) {
-            let nameTag = $(el).find('a'),
-                data = [nameTag.text()],
-                despLink = nameTag.attr('href'),
-                otherDetails = $(el).find('tr')
-
-            otherDetails.each(function (i, el) {
-                let dataValue = $(el).find('td:last-child').text()
-                data.push(dataValue)
-            });
-            outArray.push(data)
-            siteDespCrawler.queue(despLink);
-        });
-        saveToExcel(path1, outArray, 'details')
-        done();
-    }
+    maxConnections: 10
 });
 
-siteCrawler.queueSize = 1
-siteDespCrawler.queueSize = 1
+const saveCompanyDetails = async (uri, next) => {
+    if (!validUrl.isUri(uri)) return next()
+    siteDespCrawler.queue([{
+        uri,
+        callback: async (error, res, done) => {
+            if (error) return error
+            done()
+            const $ = res.$,
+                name = $('.search-page-heading-red').text(),
+                details = $('.detail-line'),
+                website = details.find('.pglink').attr('href'),
+                phone = details.clone().children().remove().end().text()
+            await saveToExcel(path2, [name, website, phone], 'website')
+            next()
+        }
+    }])
 
+}
+const saveCompany = (res, cb) => {
+    const $ = res.$,
+        out = $('.search-result-left'),
+        outArray = []
+    // traversing all results on page
+    out.each(function (i, el) {
+        let nameTag = $(el).find('a'),
+            data = [nameTag.text(), nameTag.attr('href')],
+            otherDetails = $(el).find('tr')
 
-each(range(1, 10), (item, next) => {
-    let url = `https://www.fundoodata.com/companies-in/delhi-ncr-l0?&pageno=${item}`
-    console.log(url)
-    siteCrawler.queue(url);
-    next()
+        otherDetails.each(function (i, el) {
+            data.push($(el).find('td:last-child').text())
+        });
+        outArray.push(data)
+    });
+
+    // traversing data on each result
+    each(outArray, async (item, next) => {
+        console.log(" ");
+        console.log("Getting: ", item[0])
+        await saveToExcel(path1, item, 'details')
+        // getting phone number for the desp page
+        saveCompanyDetails(item[1], next)
+    }, function (error) {
+        if (error) return error
+        cb()
+    })
+
+}
+
+each(range(101, 201), (item, next) => {
+    console.log("Reading page >>>> ", item)
+    siteCrawler.queue([{
+        uri: `https://www.fundoodata.com/companies-in/delhi-ncr-l0?&pageno=${item}`,
+        callback: async (error, res, done) => {
+            if (error) return error
+            done()
+            saveCompany(res, () => {
+                setTimeout(next, 5000)
+            })
+        }
+    }]);
 }, function (err) {
     if (err) return err
 })
+
--- saveToExcel.js
+++ saveToExcel.js
@@ -1,38 +1,34 @@
 const ExcelJS = require('exceljs');
 const each = require('async-each-series');
+const validateIt = (val) => {
+    const isValid = val !== null && val !== undefined && typeof val === 'string'
+    if (!isValid) return ""
+    return val.replace(/Click here/g, '')
+        .replace(/\t/g, '')
+        .replace(/\n/g, '')
+        .replace(": ", '')
+        .trim()
+}
 
-const addRowToNewExcel = (excelFilePath, data, type) => {
-    var workbook = new ExcelJS.Workbook();
-    workbook.xlsx.readFile(excelFilePath)
-        .then(async function () {
-            each(data, (row, next1) => {
-                let worksheet = workbook.getWorksheet(1);
-                let lastRow = worksheet.lastRow;
-                let getRowInsert = worksheet.getRow(++(lastRow.number));
-                let index = 0
-                each(['A', 'B', 'C', 'D', 'E', 'F'], (item, next2) => {
-                    if (row[index]) {
-                        row[index] = row[index]?.replace(/Click here/g, '')
-                            .replace(/\t/g, '')
-                            .replace(/\n/g, '')
-                            .replace(": ", '')
-                            .trim()
-                        getRowInsert.getCell(item).value = row[index];
-                    }
-                    index++;
-                    next2()
-                }
-                    , function (err) {
-                        console.log(`Added: ${row[0]} ${type}`);
-                        getRowInsert.commit();
-                        workbook.xlsx.writeFile(excelFilePath);
-                        next1()
-                    });
-            }, function (err) {
-                // console.log('finished');
-            });
-        });
+const addRowToNewExcel = (excelFilePath, row, type) => {
+    return new Promise((resolve, reject) => {
+        let workbook = new ExcelJS.Workbook();
+        workbook.xlsx.readFile(excelFilePath)
+            .then(async function () {
+                let worksheet = workbook.getWorksheet(1),
+                    lastRow = worksheet.lastRow,
+                    getRowInsert = worksheet.getRow(++(lastRow.number))
 
+                const validatedRow = row.map(validateIt)
+                getRowInsert.values = validatedRow
+                getRowInsert.commit();
+                return workbook.xlsx.writeFile(excelFilePath);
+            })
+            .then(() => {
+                console.log(`ADDED! ${type}`);
+                resolve()
+            })
+    })
 }
 
 module.exports = addRowToNewExcel
